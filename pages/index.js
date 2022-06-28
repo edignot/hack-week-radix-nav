ce7fccd67f0d19512d0fd38d6659f7caf049a771
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-06-28 16:08:14 -0600

    Cleaned up animations

--- pages/index.js
+++ pages/index.js
@@ -20,22 +20,6 @@ const exitToLeft = keyframes({
   from: { transform: 'translateX(0)', opacity: 1 },
   to: { transform: 'translateX(-200px)', opacity: 0 },
 })
-const scaleIn = keyframes({
-  from: { transform: 'rotateX(-30deg) scale(0.9)', opacity: 0 },
-  to: { transform: 'rotateX(0deg) scale(1)', opacity: 1 },
-})
-const scaleOut = keyframes({
-  from: { transform: 'rotateX(0deg) scale(1)', opacity: 1 },
-  to: { transform: 'rotateX(-10deg) scale(0.95)', opacity: 0 },
-})
-// const fadeIn = keyframes({
-//   from: { opacity: 0 },
-//   to: { opacity: 1 },
-// })
-// const fadeOut = keyframes({
-//   from: { opacity: 1 },
-//   to: { opacity: 0 },
-// })
 
 export const NavigationMenuDemo = () => {
   return (
@@ -47,7 +31,15 @@ export const NavigationMenuDemo = () => {
             <NavigationMenuLink> HI </NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuPrimitive.Item>
+
+        <NavigationMenuPrimitive.Item>
+          <NavigationMenuTrigger>Learn</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink> HI </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuPrimitive.Item>
       </NavigationMenuList>
+
       <ViewportPosition>
         <NavigationMenuViewport />
       </ViewportPosition>
@@ -56,42 +48,25 @@ export const NavigationMenuDemo = () => {
 }
 
 const NavigationMenu = styled(NavigationMenuPrimitive.Root, {
+  border: '2px solid red',
   position: 'relative',
   display: 'flex',
   justifyContent: 'center',
   width: '100vw',
-  zIndex: 1,
+  zIndex: 999,
 })
 
 const NavigationMenuList = styled(NavigationMenuPrimitive.List, {
-  all: 'unset',
+  border: '2px solid yellow',
+  // all: 'unset',
   display: 'flex',
   justifyContent: 'center',
-  backgroundColor: 'white',
-  padding: 4,
-  borderRadius: 6,
   listStyle: 'none',
-  boxShadow: `0 2px 10px ${blackA.blackA7}`,
+  padding: '10px',
+  zIndex: 999,
 })
 
-const StyledTrigger = styled(NavigationMenuPrimitive.Trigger, {
-  all: 'unset',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  gap: 2,
-})
-
-const StyledCaret = styled(CaretDownIcon, {
-  position: 'relative',
-  color: violet.violet10,
-  top: 1,
-  '[data-state=open] &': { transform: 'rotate(-180deg)' },
-  '@media (prefers-reduced-motion: no-preference)': {
-    transition: 'transform 250ms ease',
-  },
-})
-
+// TRIGGER
 const NavigationMenuTrigger = React.forwardRef(
   ({ children, ...props }, forwardedRef) => (
     <StyledTrigger {...props} ref={forwardedRef}>
@@ -101,11 +76,34 @@ const NavigationMenuTrigger = React.forwardRef(
   )
 )
 
+const StyledTrigger = styled(NavigationMenuPrimitive.Trigger, {
+  border: '2px solid yellow',
+  all: 'unset',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: 2,
+  color: 'white',
+})
+
+const StyledCaret = styled(CaretDownIcon, {
+  border: '2px solid yellow',
+  position: 'relative',
+  color: 'white',
+  '[data-state=open] &': { transform: 'rotate(-180deg)' },
+  '@media (prefers-reduced-motion: no-preference)': {
+    transition: 'transform 250ms ease',
+  },
+})
+// TRIGGER //
+
 const NavigationMenuContent = styled(NavigationMenuPrimitive.Content, {
   position: 'absolute',
   // top: 0,
   // left: 0,
   width: '100vw',
+  height: 'auto',
+  minHeight: '30vh',
   '@media (prefers-reduced-motion: no-preference)': {
     animationDuration: '250ms',
     animationTimingFunction: 'ease',
@@ -123,24 +121,10 @@ const NavigationMenuLink = styled(NavigationMenuPrimitive.Link, {
 
 const NavigationMenuViewport = styled(NavigationMenuPrimitive.Viewport, {
   position: 'relative',
-  transformOrigin: 'top center',
-  marginTop: 10,
   width: '100%',
   backgroundColor: 'white',
-  borderRadius: 6,
   overflow: 'hidden',
-  boxShadow:
-    'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
   height: 'var(--radix-navigation-menu-viewport-height)',
-
-  '@media only screen and (min-width: 600px)': {
-    width: 'var(--radix-navigation-menu-viewport-width)',
-  },
-  '@media (prefers-reduced-motion: no-preference)': {
-    transition: 'width, height, 300ms ease',
-    '&[data-state="open"]': { animation: `${scaleIn} 200ms ease` },
-    '&[data-state="closed"]': { animation: `${scaleOut} 200ms ease` },
-  },
 })
 
 const ViewportPosition = styled('div', {
